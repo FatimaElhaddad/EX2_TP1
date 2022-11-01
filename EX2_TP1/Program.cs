0d5f5ef39e6be2c1341959a9a963c5f582b11090
FatimaELhaddad <fatima.elhaddad3@usmba.ac.ma>
2022-11-01 22:14:36 +0100

    Ajoutez des fichiers projet.

---
+++ EX2_TP1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EXERCICE2
+{
+    class Program
+    {
+
+        static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("[" + arr[i] + "] ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("N = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("*******************");
+            int[] arr = { 5, 20, 25, 34, 35 };
+            Print(arr);
+            Array.Resize(ref arr, arr.Length + 1);
+            int size = arr.Length;
+            arr[size - 1] = n;
+            Console.WriteLine("\n*******************");
+            for (int i = size - 1; i > 0; i--)
+            {
+                int m;
+                if (n < arr[i - 1])
+                {
+                    m = arr[i - 1];
+                    arr[i - 1] = arr[i];
+                    arr[i] = m;
+                }
+            }
+            Print(arr);
+        }
+    }
+}
